0706505b29835e4e2b973c7fe6d8107cdd52db52
priyans-hu <gargpriyanshu2004@gmail.com>
2024-07-03 13:54:29 +0530

    updated app.js

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,11 @@ import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import ComingSoon from './pages/comingSoon.js';
 import PageNotFound from './pages/pageNotFound.js';
+import Dashboard from './pages/dashboard.js';
+import AddCustomer from './pages/addCustomer.js';
+import ViewCustomers from './pages/viewCustomers.js';
+import AddTransaction from './pages/addTransaction.js';
+import ManageTransactions from './pages/manageTransactions.js';
 
 function App() {
   return (
@@ -16,10 +21,14 @@ function App() {
         <Router>
           <Routes>
             <Route path="/" element={<LandingPage />} />
-            <Route path="/dashboard" element={<ComingSoon />} />
+            <Route path="/dashboard" element={<Dashboard />} />
             <Route path="/login" element={<Login />} />
             <Route path="/signup" element={<Signup />} />
             <Route path="/generate-invoice" element={<ComingSoon />} />
+            <Route path="/add-customer" element={<AddCustomer />} />
+            <Route path="/view-customers" element={<ViewCustomers />} />
+            <Route path="/add-transaction" element={<AddTransaction />} />
+            <Route path="/manage-transactions" element={<ManageTransactions />} />
             <Route path="*" element={<PageNotFound />} />
           </Routes>
         </Router>
