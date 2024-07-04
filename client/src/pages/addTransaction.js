907452142493810c0d6462e427f1731dfa61ab2f
priyans-hu <gargpriyanshu2004@gmail.com>
2024-07-04 17:22:28 +0530

    completed transaction pages

--- client/src/pages/addTransaction.js
+++ client/src/pages/addTransaction.js
@@ -1,14 +1,148 @@
-import React from 'react'
-import Header from '../components/Header'
-import Footer from '../components/Footer'
+import React from 'react';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+import { TextField, Button, MenuItem } from '@mui/material';
+import transactionApi from '../api/transactionApi';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import { toast } from 'react-toastify';
 
-const addTransaction = () => {
+const AddTransaction = () => {
+    const formik = useFormik({
+        initialValues: {
+            amount: '',
+            description: '',
+            date: '',
+            type: '',
+            method: '',
+            customerid: ''
+        },
+        validationSchema: Yup.object({
+            amount: Yup.number()
+                .required('Amount is required')
+                .integer('Amount must be a valid integer')
+                .positive('Amount must be a positive number'),
+            type: Yup.string().required('Type is required'),
+            method: Yup.string().required('Method is required'),
+            customerid: Yup.number()
+                .integer('Customer ID must be a valid integer')
+                .positive('Customer ID must be a positive number'),
+        }),
+        onSubmit: async (values) => {
+            try {
+                await transactionApi.createTransaction(values);
+                toast.success('Transaction added successfully');
+            } catch (error) {
+                toast.error('Failed to add transaction');
+            }
+        }
+    });
+
     return (
-        <div>
+        <>
             <Header />
+            <div className="container mx-auto my-16 p-4 max-w-md min-h-[80vh]">
+                <h1 className="text-4xl text-center font-bold mb-8">Add Transaction</h1>
+                <div className='h-full flex items-center justify-center p-8 bg-gray-100 rounded-lg'>
+                    <form onSubmit={formik.handleSubmit} className="space-y-4">
+                        <TextField
+                            fullWidth
+                            id="amount"
+                            name="amount"
+                            label={
+                                <>
+                                    Amount <span className="text-red-500">*</span>
+                                </>
+                            }
+                            value={formik.values.amount}
+                            onChange={formik.handleChange}
+                            error={formik.touched.amount && Boolean(formik.errors.amount)}
+                            helperText={formik.touched.amount && formik.errors.amount}
+                        />
+                        <TextField
+                            fullWidth
+                            id="description"
+                            name="description"
+                            label="Description"
+                            value={formik.values.description}
+                            onChange={formik.handleChange}
+                        />
+                        <TextField
+                            fullWidth
+                            id="date"
+                            name="date"
+                            label={
+                                <>
+                                    Date <span className="text-red-500">*</span>
+                                </>
+                            }
+                            type="date"
+                            value={formik.values.date}
+                            onChange={formik.handleChange}
+                            InputLabelProps={{
+                                shrink: true,
+                            }}
+                        />
+                        <TextField
+                            fullWidth
+                            id="type"
+                            name="type"
+                            label={
+                                <>
+                                    Type <span className="text-red-500">*</span>
+                                </>
+                            }
+                            select
+                            value={formik.values.type}
+                            onChange={formik.handleChange}
+                            error={formik.touched.type && Boolean(formik.errors.type)}
+                            helperText={formik.touched.type && formik.errors.type}
+                        >
+                            <MenuItem value="credit">Credit</MenuItem>
+                            <MenuItem value="debit">Debit</MenuItem>
+                        </TextField>
+                        <TextField
+                            fullWidth
+                            id="method"
+                            name="method"
+                            label={
+                                <>
+                                    Method <span className="text-red-500">*</span>
+                                </>
+                            }
+                            select
+                            value={formik.values.method}
+                            onChange={formik.handleChange}
+                            error={formik.touched.method && Boolean(formik.errors.method)}
+                            helperText={formik.touched.method && formik.errors.method}
+                        >
+                            <MenuItem value="cash">Cash</MenuItem>
+                            <MenuItem value="card">Card</MenuItem>
+                            <MenuItem value="upi">Upi</MenuItem>
+                        </TextField>
+                        <TextField
+                            fullWidth
+                            id="customerid"
+                            name="customerid"
+                            label={
+                                <>
+                                    Customer ID <span className="text-red-500">*</span>
+                                </>
+                            }
+                            value={formik.values.customerid}
+                            onChange={formik.handleChange}
+                            error={formik.touched.customerid && Boolean(formik.errors.customerid)}
+                            helperText={formik.touched.customerid && formik.errors.customerid}
+                        />
+                        <Button color="primary" variant="contained" fullWidth type="submit">
+                            Add Transaction
+                        </Button>
+                    </form>
+                </div>
+            </div>
             <Footer />
-        </div>
-    )
-}
+        </>
+    );
+};
 
-export default addTransaction
+export default AddTransaction;
--- client/src/pages/manageTransactions.js
+++ client/src/pages/manageTransactions.js
@@ -1,14 +1,213 @@
-import React from 'react'
-import Header from '../components/Header'
-import Footer from '../components/Footer'
+import React, { useState, useEffect } from 'react';
+import { TextField, Button, TableContainer, Paper, Menu, MenuItem, ListItemText } from '@mui/material';
+import transactionApi from '../api/transactionApi';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import { Link } from 'react-router-dom';
 
-const manageTransactions = () => {
+const ManageTransactions = () => {
+    const [transactions, setTransactions] = useState([]);
+    const [date, setDate] = useState('');
+    const [monthlyTotal, setMonthlyTotal] = useState(0);
+    const [sortOption, setSortOption] = useState('dateNewToOld');
+    const [anchorEl, setAnchorEl] = useState(null);
+
+    useEffect(() => {
+        fetchTransactions();
+    }, []);
+
+    const fetchTransactions = async () => {
+        try {
+            const response = await transactionApi.getAllTransactions();
+            const transactions = response.data.transactions || [];
+            setTransactions(transactions);
+            calculateMonthlyTotal(transactions);
+        } catch (error) {
+            alert('Failed to fetch transactions');
+        }
+    };
+
+    const fetchTransactionsByDate = async () => {
+        try {
+            const response = await transactionApi.getTransactionsByDate(date);
+            const transactions = response.data.transactions || [];
+            setTransactions(transactions);
+            calculateMonthlyTotal(transactions);
+        } catch (error) {
+            alert('Failed to fetch transactions by date');
+        }
+    };
+
+    const calculateMonthlyTotal = (transactions) => {
+        const currentMonth = new Date().getMonth();
+        const currentYear = new Date().getFullYear();
+        const monthlyTransactions = transactions.filter(transaction => {
+            const transactionDate = new Date(transaction.date);
+            return transactionDate.getMonth() === currentMonth && transactionDate.getFullYear() === currentYear;
+        });
+
+        const total = monthlyTransactions.reduce((acc, transaction) => {
+            const amount = parseFloat(transaction.amount);
+            return acc + (transaction.type === 'credit' ? amount : -amount);
+        }, 0);
+
+        setMonthlyTotal(total.toFixed(2));
+    };
+
+    const formatDate = (dateString) => {
+        const date = new Date(dateString);
+        const day = String(date.getDate()).padStart(2, '0');
+        const month = String(date.getMonth() + 1).padStart(2, '0');
+        const year = date.getFullYear();
+        return `${day}/${month}/${year}`;
+    };
+
+    const handleSortOption = (option) => {
+        setSortOption(option);
+        handleSortTransactions(option);
+    };
+
+    const handleSortTransactions = (option) => {
+        let sortedTransactions = [...transactions];
+
+        switch (option) {
+            case 'dateNewToOld':
+                sortedTransactions.sort((a, b) => new Date(b.date) - new Date(a.date));
+                break;
+            case 'dateOldToNew':
+                sortedTransactions.sort((a, b) => new Date(a.date) - new Date(b.date));
+                break;
+            case 'amountHighToLow':
+                sortedTransactions.sort((a, b) => parseFloat(b.amount) - parseFloat(a.amount));
+                break;
+            case 'amountLowToHigh':
+                sortedTransactions.sort((a, b) => parseFloat(a.amount) - parseFloat(b.amount));
+                break;
+            default:
+                break;
+        }
+
+        setTransactions(sortedTransactions);
+    };
+
+    const openMenu = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const closeMenu = () => {
+        setAnchorEl(null);
+    };
+
+    const resetFilters = () => {
+        fetchTransactions();
+        setDate('');
+    };
+
     return (
-        <div>
+        <>
             <Header />
+            <div className="container mx-auto my-16 p-4 min-h-[80vh]">
+                <h1 className="text-4xl text-center font-bold mb-8">Manage Transactions</h1>
+                <div className="flex flex-col md:flex-row md:space-x-4 mb-4">
+                    <TextField
+                        id="date"
+                        label="Date"
+                        type="date"
+                        InputLabelProps={{
+                            shrink: true,
+                        }}
+                        value={date}
+                        onChange={(e) => setDate(e.target.value)}
+                        className="mb-4 md:mb-0"
+                    />
+                    <Button
+                        variant="contained"
+                        color="primary"
+                        onClick={fetchTransactionsByDate}
+                        className="mb-4 md:mb-0 md:mr-2"
+                    >
+                        Filter by Date
+                    </Button>
+                    {date && (
+                        <Button variant="contained" onClick={resetFilters} className="mb-4 md:mb-0 md:mr-2">
+                            Reset Filters
+                        </Button>
+                    )}
+                    <Button variant="contained" onClick={fetchTransactions} className="mb-4 md:mb-0 md:mr-2">
+                        Show All
+                    </Button>
+                    <Button
+                        variant="contained"
+                        color="secondary"
+                        aria-controls="sort-menu"
+                        aria-haspopup="true"
+                        onClick={openMenu}
+                    >
+                        Sort By
+                    </Button>
+                    <Menu
+                        id="sort-menu"
+                        anchorEl={anchorEl}
+                        keepMounted
+                        open={Boolean(anchorEl)}
+                        onClose={closeMenu}
+                    >
+                        <MenuItem onClick={() => handleSortOption('dateNewToOld')}>
+                            <ListItemText primary="Date: New to Old" />
+                        </MenuItem>
+                        <MenuItem onClick={() => handleSortOption('dateOldToNew')}>
+                            <ListItemText primary="Date: Old to New" />
+                        </MenuItem>
+                        <MenuItem onClick={() => handleSortOption('amountHighToLow')}>
+                            <ListItemText primary="Amount: High to Low" />
+                        </MenuItem>
+                        <MenuItem onClick={() => handleSortOption('amountLowToHigh')}>
+                            <ListItemText primary="Amount: Low to High" />
+                        </MenuItem>
+                    </Menu>
+                </div>
+                <TableContainer component={Paper} className="overflow-x-auto">
+                    <table className="min-w-full bg-white border border-gray-200 shadow-md rounded-lg overflow-hidden">
+                        <thead className="bg-gray-100 text-gray-800">
+                            <tr>
+                                <td className="px-6 py-3 text-left font-bold">Serial No</td>
+                                <td className="px-6 py-3 text-left font-bold">Amount</td>
+                                <td className="px-6 py-3 text-left font-bold">Description</td>
+                                <td className="px-6 py-3 text-left font-bold">Date</td>
+                                <td className="px-6 py-3 text-left font-bold">Type</td>
+                                <td className="px-6 py-3 text-left font-bold">Method</td>
+                                <td className="px-6 py-3 text-left font-bold">Customer ID</td>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {transactions.map((transaction, index) => (
+                                <tr key={transaction.transactionid}>
+                                    <td className="px-6 py-4 whitespace-nowrap">{index + 1}</td>
+                                    <td className="px-6 py-4 whitespace-nowrap" style={{ color: transaction.type === 'credit' ? 'green' : 'red' }}>{transaction.amount}</td>
+                                    <td className="px-6 py-4 whitespace-nowrap">{transaction.description}</td>
+                                    <td className="px-6 py-4 whitespace-nowrap">{formatDate(transaction.date)}</td>
+                                    <td className="px-6 py-4 whitespace-nowrap">{transaction.type}</td>
+                                    <td className="px-6 py-4 whitespace-nowrap">{transaction.method}</td>
+                                    <td className="px-6 py-4 whitespace-nowrap">{transaction.customerid || '-'}</td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </TableContainer>
+                <div className="flex justify-end mt-4">
+                    <h2 className="text-2xl font-bold">Monthly Total: {monthlyTotal}</h2>
+                </div>
+                <div className="flex justify-end mt-4">
+                    <Link to='/add-transaction'>
+                        <Button variant="contained" color="secondary">
+                            Add Transaction
+                        </Button>
+                    </Link>
+                </div>
+            </div>
             <Footer />
-        </div>
-    )
-}
+        </>
+    );
+};
 
-export default manageTransactions
+export default ManageTransactions;
